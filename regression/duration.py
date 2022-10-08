2323f75a50c506b4b16ff1df7052bd8cae68260a
Michael Berkowitz <michael.berkowitz@gmail.com>
2022-10-07 23:14:20 -0400

    Add data for Duration.multiply and divide

--- regression/duration.py
+++ regression/duration.py
@@ -59,7 +59,7 @@ def write_duration_add_data():
                 str(dur_sum.denominator)
             ]))
             f.write("\n")
-        if idx > 0 and (idx % 10000 == 0 or idx == pairs_len):
+        if idx > 0 and (idx % 100000 == 0 or idx == pairs_len):
             print("  {}/{}".format(idx, pairs_len))
     f.close()
 
@@ -84,7 +84,103 @@ def write_duration_subtract_data():
                 str(dur_diff.denominator)
             ]))
             f.write("\n")
-        if idx > 0 and (idx % 10000 == 0 or idx == pairs_len):
+        if idx > 0 and (idx % 100000 == 0 or idx == pairs_len):
+            print("  {}/{}".format(idx, pairs_len))
+    f.close()
+
+
+def write_duration_multiply_data():
+    print("Generating data for Duration.multiply")
+    f = open("data/duration/multiply.txt", "w")
+    pairs = [(i1, i2) for i1 in durations(15) for i2 in durations(15)]
+    pairs_len = len(pairs)
+    print(pairs_len)
+    for (idx, ((n, d), (n2, d2))) in enumerate(pairs):
+        if d != 0 and d2 != 0:
+            dur1 = abjad.Duration(n, d)
+            dur2 = abjad.Duration(n2, d2)
+            product = dur1 * dur2
+            f.write(":".join([
+                str(n),
+                str(d),
+                str(n2),
+                str(d2),
+                str(product.numerator),
+                str(product.denominator)
+            ]))
+            f.write("\n")
+        if idx > 0 and (idx % 100000 == 0 or idx == pairs_len):
+            print("  {}/{}".format(idx, pairs_len))
+    f.close()
+
+
+def write_duration_divide_data():
+    print("Generating data for Duration.divide")
+    f = open("data/duration/divide.txt", "w")
+    pairs = [(i1, i2) for i1 in durations(15) for i2 in durations(15)]
+    pairs_len = len(pairs)
+    print(pairs_len)
+    for (idx, ((n, d), (n2, d2))) in enumerate(pairs):
+        if d != 0 and n2 != 0 and d2 != 0:
+            dur1 = abjad.Duration(n, d)
+            dur2 = abjad.Duration(n2, d2)
+            dividend = dur1 / dur2
+            f.write(":".join([
+                str(n),
+                str(d),
+                str(n2),
+                str(d2),
+                str(dividend.numerator),
+                str(dividend.denominator)
+            ]))
+            f.write("\n")
+        if idx > 0 and (idx % 100000 == 0 or idx == pairs_len):
+            print("  {}/{}".format(idx, pairs_len))
+    f.close()
+
+
+def write_duration_multiply_by_int_data():
+    print("Generating data for Duration.multiply (by an integer)")
+    f = open("data/duration/multiply_by_int.txt", "w")
+    pairs = [(d, i) for d in durations(15) for i in range(-15, 15)]
+    pairs_len = len(pairs)
+    print(pairs_len)
+    for (idx, ((n, d), i)) in enumerate(pairs):
+        if d != 0:
+            dur = abjad.Duration(n, d)
+            product = dur * i
+            f.write(":".join([
+                str(n),
+                str(d),
+                str(i),
+                str(product.numerator),
+                str(product.denominator)
+            ]))
+            f.write("\n")
+        if idx > 0 and (idx % 100000 == 0 or idx == pairs_len):
+            print("  {}/{}".format(idx, pairs_len))
+    f.close()
+
+
+def write_duration_divide_by_int_data():
+    print("Generating data for Duration.divide (by an integer)")
+    f = open("data/duration/divide_by_int.txt", "w")
+    pairs = [(d, i) for d in durations(15) for i in range(-15, 15)]
+    pairs_len = len(pairs)
+    print(pairs_len)
+    for (idx, ((n, d), i)) in enumerate(pairs):
+        if d != 0 and i != 0:
+            dur = abjad.Duration(n, d)
+            dividend = dur / i
+            f.write(":".join([
+                str(n),
+                str(d),
+                str(i),
+                str(dividend.numerator),
+                str(dividend.denominator)
+            ]))
+            f.write("\n")
+        if idx > 0 and (idx % 100000 == 0 or idx == pairs_len):
             print("  {}/{}".format(idx, pairs_len))
     f.close()
 
@@ -95,3 +191,7 @@ def generate_data():
     write_duration_printable_data()
     write_duration_add_data()
     write_duration_subtract_data()
+    write_duration_multiply_data()
+    write_duration_divide_data()
+    write_duration_multiply_by_int_data()
+    write_duration_divide_by_int_data()
