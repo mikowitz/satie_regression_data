9c349266c4cd5506dcae9504eb061ca356afa163
Michael Berkowitz <michael.berkowitz@gmail.com>
2022-10-07 22:57:09 -0400

    Add duration subtract data

--- regression/duration.py
+++ regression/duration.py
@@ -64,8 +64,34 @@ def write_duration_add_data():
     f.close()
 
 
+def write_duration_subtract_data():
+    print("Generating data for Duration.subtract")
+    f = open("data/duration/subtract.txt", "w")
+    pairs = [(i1, i2) for i1 in durations(15) for i2 in durations(15)]
+    pairs_len = len(pairs)
+    print(pairs_len)
+    for (idx, ((n, d), (n2, d2))) in enumerate(pairs):
+        if d != 0 and d2 != 0:
+            dur1 = abjad.Duration(n, d)
+            dur2 = abjad.Duration(n2, d2)
+            dur_diff = dur1 - dur2
+            f.write(":".join([
+                str(n),
+                str(d),
+                str(n2),
+                str(d2),
+                str(dur_diff.numerator),
+                str(dur_diff.denominator)
+            ]))
+            f.write("\n")
+        if idx > 0 and (idx % 10000 == 0 or idx == pairs_len):
+            print("  {}/{}".format(idx, pairs_len))
+    f.close()
+
+
 def generate_data():
     os.makedirs("data/duration", exist_ok=True)
     write_duration_new_data()
     write_duration_printable_data()
     write_duration_add_data()
+    write_duration_subtract_data()
